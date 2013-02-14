e1ab2eb380556c5f452593ec166444e30e1b47f9
Tobias Quinn <tobias@tobiasquinn.com>
2013-02-14 04:52:34 +0000

    reads time of vote from server

--- votecount/app/scripts/controllers/main.js
+++ votecount/app/scripts/controllers/main.js
@@ -1,21 +1,25 @@
 'use strict';
 
-votecountApp.controller('MainCtrl', function($scope, $timeout) {
+votecountApp.controller('MainCtrl', function($scope, $timeout, $http) {
     $scope.yourvote = false;
     $scope.votes = 'UNKNOWN';
     $scope.timeleft = 60;
 
     $scope.vote = function(vote) {
         $scope.yourvote = vote;
-        $scope.count[vote]++;
     };
 
     var interval = 1000;
+    $http.get('/votingtime').success(function(data) {
+        console.log("GOT");
+        console.log(data);
+        $scope.timeleft = data['time'];
+        var mytimeout = $timeout($scope.onTimeout, interval);
+    });
     $scope.onTimeout = function() {
         $scope.timeleft--;
         if ($scope.timeleft > 0) {
-            mytimeout = $timeout($scope.onTimeout, interval);
+            var mytimeout = $timeout($scope.onTimeout, interval);
         }
     }
-    var mytimeout = $timeout($scope.onTimeout, interval);
 });
--- votecount/server.py
+++ votecount/server.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, jsonify
+import random
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': '/index.html'})
 @app.route('/<path:path>')
 def catch_all(path):
     return send_file('app/%s' % (path))
+
+@app.route('/votingtime')
+def votingtime():
+    return jsonify(time=random.randint(10,50))
 
 if __name__ == '__main__':
     app.debug = True
