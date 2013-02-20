ca7e61c01aefda7f1269126eeb03a7c351a6dd7f
Tobias Quinn <tobias@tobiasquinn.com>
2013-02-20 04:14:11 +0000

    changed bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 rm -rf venv
 virtualenv --system-site-packages venv
 source venv/bin/activate
-pip install tornado
+pip install tornado tornadio2 sockjs-tornado
 echo Activate with source venv/bin/activate
